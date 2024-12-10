a86efa30ccdb530d5efaaa5e92125bf6acb2be1b
talhaansari04 <talhaansari61@gmail.com>
2024-12-11 01:30:45 +0530

    Fixed Test Config

--- src/main/groovy/com/igse/IgsePlugin.groovy
+++ src/main/groovy/com/igse/IgsePlugin.groovy
@@ -8,10 +8,11 @@ import org.gradle.api.tasks.testing.Test
 
 class IgsePlugin implements Plugin<Project> {
     String IGSE_TEST = "igse-test"
+    static final String MSG = "Igse plugin begin-" + UUID.randomUUID().toString().substring(0, 3)
 
     @Override
     void apply(Project project) {
-        println("Igse plugin begin")
+        println(MSG)
         addStubRunner(project)
         addTestingTask(project)
     }
@@ -24,22 +25,33 @@ class IgsePlugin implements Plugin<Project> {
 
     void addTestingTask(Project project) {
         project.plugins.withType(JavaPlugin) {
-            /*project.test{
-     include 'unit/**'
-     useJunit
- }*/
+
+            project.test {
+                include '**/unit/**'
+                useJUnitPlatform()
+            }
+
             project.tasks.register('componentTest', Test.class) {
                 group = IGSE_TEST
-                //mustRunAfter:project.test
-                include "component/**"
+                mustRunAfter:
+                project.test
+                include '**/component/**'
+                exclude '**/unit/**'
                 useJUnitPlatform()
             }
-            project.tasks.register('blackBoxTest', Test.class) {
+            project.tasks.register('blackboxTest', Test.class) {
                 group = IGSE_TEST
-                //mustRunAfter:project.test
-                include "blackBox/**"
-                useJUnitPlatform()
+                mustRunAfter:
+                project.componentTest
+                include '**/blackbox/**'
+                exclude '**/unit/**'
+                // useJUnitPlatform()
+                testLogging.showStandardStreams = true
             }
+            //project.componentTest.mustRunAfter project.test
+            project.check.dependsOn project.componentTest
+            project.check.dependsOn project.blackboxTest
+//            project.build.dependsOn project.blackboxTest
             project.tasks.withType(Test) {
                 reports.html.destination = project.file("${project.reporting.baseDir}/${name}")
                 testLogging {
